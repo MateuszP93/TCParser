0a09ec9477c1bcba67ff4c9ad0542c0063fbfbd4
paulewim <paulewicz.mateusz@gmail.com>
2023-07-17 21:13:47 +0200

    Added option for parse step code

--- main.py
+++ main.py
@@ -23,30 +23,32 @@ class App(customtkinter.CTk):
     HEIGHT = 700
     current_line = ""
     old_template = False
+    errorList = []  # line number, error description
 
-    options = ["Verify Coding",
-               "Remove unnecessary lines",
-               "Excessive Spacebards",
-               "Indentation step level",
-               "Step finished with dot",
+    options = [
+        "Update Step level",
+        "Verify Coding",
+        "Remove unnecessary lines",
+        "Excessive Spacebards",
+        "Indentation step level",
+        "Step finished with dot",
 
-               "Verify ASCII chars range",
-               "Validation of headers",
-               "Use of single quotes",
-               "Not closed quotes",
-               "Indentation of Steps",
-               "Variables in Steps",
+        "Verify ASCII chars range",
+        "Validation of headers",
+        "Use of single quotes",
+        "Not closed quotes",
+        "Variables in Steps",
 
-               "Not closed backets",
-               "Binary operators correctness",
-               "Semicolor termination",
-               "Check imports",
-               "Line length < 200",
-               "While loop",
-               "Bad practise naming",
-               "Tabs not spaces",
-               "General indentation",
-               "Template updater"]
+        "Not closed backets",
+        "Binary operators correctness",
+        "Semicolor termination",
+        "Check imports",
+        "Line length < 200",
+        "While loop",
+        "Bad practise naming",
+        "Tabs not spaces",
+        "General indentation",
+        "Template updater"]
 
     def __init__(self):
         self.lastDir = self.GetOptionFromCfg(self.configFilePath, "DEFAULT", "last directory")
@@ -90,14 +92,23 @@ class App(customtkinter.CTk):
         self.checkbox_dict = {}
         for index, option_name in enumerate(self.options):
             checkbox = customtkinter.CTkCheckBox(master=self.select_grid, text=f"{option_name}", command=self.check_box_update_event)
-            checkbox.grid(row=index // 3, column=index % 3, pady=10, padx=10, sticky="nwe")
+            checkbox.grid(row=index // 3, column=index % 3, pady=8, padx=10, sticky="nwe")
+            print(option_name)
             checkbox.select() if int(self.GetOptionFromCfg(self.configFilePath, section="DEFAULT", option=option_name)) else checkbox.deselect()
             self.checkbox_dict.update({option_name: checkbox})
+
+        # ====================== select section settings ======================
+
+        self.select_version_grid = customtkinter.CTkFrame(master=self)
+        self.select_version_grid.grid(row=3, column=0, sticky="wesn", padx=10, pady=(10, 0))
+        self.checkbox_version = customtkinter.CTkCheckBox(master=self.select_version_grid, text=f"Old tc version", command=self.check_box_update_event)
+        self.checkbox_version.grid(row=1, column=1, pady=10, padx=10, sticky="nwe")
+        self.checkbox_version.select() if int(self.GetOptionFromCfg(self.configFilePath, section="DEFAULT", option="tc version")) else self.checkbox_version.deselect()
 
         # ====================== footer section settings ======================
 
         self.footer_grid = customtkinter.CTkFrame(master=self)
-        self.footer_grid.grid(row=3, column=0, sticky="swe", padx=10, pady=(10, 0))
+        self.footer_grid.grid(row=4, column=0, sticky="swe", padx=10, pady=(10, 0))
         self.footer_grid.grid_columnconfigure(0, weight=1)
         self.select_catalog_button = customtkinter.CTkButton(master=self.footer_grid,
                                                              text="PARSE TEST CASE",
@@ -108,11 +119,15 @@ class App(customtkinter.CTk):
         pass
 
     def on_closing(self):
+        self._update_init_file()
+        self.destroy()
+
+    def _update_init_file(self):
         for key_checkbox, item_checkbox in self.checkbox_dict.items():
             self.SaveOptToCfg(self.configFilePath, section="DEFAULT", option=key_checkbox.lower(), value=str(item_checkbox.get()))
         self.SaveOptToCfg(self.configFilePath, section="DEFAULT", option="last directory", value=str(self.lastDir))
+        self.SaveOptToCfg(self.configFilePath, section="DEFAULT", option="tc version", value=str(self.checkbox_version.get()))
         self.SaveOptToCfg(self.configFilePath, section="DEFAULT", option="template directory", value=str(self.live_template_path))
-        self.destroy()
 
     def select_catalog_event(self):
         self.textbox.delete("0.0", "end")
@@ -140,6 +155,7 @@ class App(customtkinter.CTk):
         self.textbox.insert("0.0", temp_text)
 
     def start_parse(self):
+        self._update_init_file()
         for file_open_path in self.file_to_parse:
             temp_data_line = []
             with open(file_open_path, "r") as open_file:
@@ -164,8 +180,9 @@ class App(customtkinter.CTk):
 
                 temp_data_line.append(self.current_line)
             self.temporary_file = temp_data_line
+            if self.GetOptionFromCfg(self.configFilePath, "DEFAULT", "update step level"):
+                self.parse_step_level_file()
 
-            self.ParseFile()
             with open(file_open_path, "w") as open_file:
                 open_file.writelines(self.temporary_file)
 
@@ -234,61 +251,17 @@ class App(customtkinter.CTk):
             temporary_file = re.sub("\n{2,}", "\n\n", temporary_file)
             self.temporary_file = [each_line + "\n" for each_line in temporary_file.split("\n")]
 
-    def ParseFile(self, ):
-        #     status = 'OK'
+    def parse_step_level_file(self, ):
         MAX_STEP_LEVEL = 3
         tcLines = self.temporary_file
 
         stepNumber = MAX_STEP_LEVEL * [0]  # counter for steps numerating
-        logsNumber = 0
         procedure = '\n##    PROCEDURE:\n'  # procedure string
-        procedureFlag = False  # True during procedure lines parsing, False when parsing complete
-        reqirementFlag = False  # True when requrements are parsed
         parsedLines = []
-        headerEndLineNumber = 0
-        errorList = []  # line number, error description
-        lineNumberOffset = 0
 
+        OldTestCaseStyle = self.checkbox_version.get()
         for lineNumber, line in enumerate(tcLines, 1):
-            if not headerEndLineNumber:
-                if line.startswith('""")'):
-                    headerEndLineNumber = lineNumber
-                elif line.startswith('header.Log()'):
-                    headerEndLineNumber = lineNumber + 1
-
-            if not line.startswith('#') and line.count("'") and (not line.count('"') or line.find("'") < line.find('"')):
-                errorList.append(
-                    [lineNumber, ' - single quote(s) instead of double quote(s): strings shall be in double quotes ["].'])
-                status = 'NOK'
-
-            elif not line.startswith('#') and line.count('"') % 2 and not line.count('"""'):
-                errorList.append([lineNumber, ' - lack quotation mark(s): strings shall be in quotation marks ["].'])
-                status = 'NOK'
-
-            elif re.search(r'^#* *Req *\(', line):
-                reqirementFlag = True
-                if not line.startswith('#'):  # process only not hashed lines
-                    line = re.sub(r'Req *\( *\[ *" *', 'Req(["', line,
-                                  1)  # delete needless space chars in `Req ( [ " ` string
-                    line = re.sub(r' *" *\] *\) *$', '"])', line, 1)  # delete needless space chars in ` " ] ) ` string
-                    line = re.sub(r' *" *, *" *', '", "', line)  # delete needless space chars in ` " , " ` string
-
-                    if not re.search(r'Req\(\[.*\]\)', line):
-                        errorList.append([lineNumber, ' - lack bracket: requirements shall be list of strings.'])
-                        status = 'NOK'
-                    else:
-                        requirements = re.search(r'(?<=\[).*(?=\])', line).group()
-                        bordersCheck = re.search(r'(^[^"])|([^"]$)', requirements)  # check if starts and ends with "
-                        oneQuotationMarkCheck = re.search(r'(^"$)',
-                                                          requirements)  # check if only one " (quotation mark) is used
-                        internalCheck = re.search(r'([^"], )|(, [^"])',
-                                                  requirements)  # check if , (comma) is rounded by " (quotation marks)
-                        if bordersCheck or oneQuotationMarkCheck or internalCheck:
-                            errorList.append(
-                                [lineNumber, ' - lack quotation mark(s): requirements shall be list of strings.'])
-                            status = 'NOK'
-
-            elif re.search(r'^[ \t]*Step *\(', line) or re.search(r'^[ \t]*with Step *\(', line):
+            if re.search(r'^[ \t]*Step *\(', line) or re.search(r'^[ \t]*with Step *\(', line):
                 try:
                     indent = re.search(r'^[ \t]*', line).group()
                     level = re.search(r'" *, *(\d+) *\)', line)
@@ -299,7 +272,7 @@ class App(customtkinter.CTk):
                         level = 1
                         levelStr = ''
                     if level > MAX_STEP_LEVEL:
-                        errorList.append(
+                        self.errorList.append(
                             [lineNumber, ' - wrong level value: level parameter shall be one of integers 1, 2 or 3.'])
                         status = 'NOK'
                     else:
@@ -313,56 +286,27 @@ class App(customtkinter.CTk):
                                 stepText += str(stepNumber[i]) + '.'
                             else:
                                 stepNumber[i] = 0
-                        # if OldTestCaseStyle == 0:
-                        #     if line.startswith("Step("):
-                        #         print(f"You have selected New TC's ABC model, line: {lineNumber} "
-                        #               "Incorrect naming, use 'with Step(' in your TC's instead")
-                        #     else:
-                        #         line = indent + 'with Step("' + stepText + text + '"' + levelStr + '):\n'
-                        # else:
-                        #     if OldTestCaseStyle == 1:
-                        #         if line.startswith("with Step(") or line.startswith("    "):
-                        #             print(f"You have selected OLD TC's model, line: {lineNumber} "
-                        #                   "Incorrect naming, use 'Step(' in your TC's instead")
-                        #         else:
-                        #             line = indent + 'Step("' + stepText + text + '"' + levelStr + ')\n'
+                        if OldTestCaseStyle:
+                            if line.startswith("Step("):
+                                print(f"You have selected New TC's ABC model, line: {lineNumber} "
+                                      "Incorrect naming, use 'with Step(' in your TC's instead")
+                            else:
+                                line = indent + 'with Step("' + stepText + text + '"' + levelStr + '):\n'
+                        else:
+                            if line.startswith("with Step(") or line.startswith("    "):
+                                print(f"You have selected OLD TC's model, line: {lineNumber} "
+                                      "Incorrect naming, use 'Step(' in your TC's instead")
+                            else:
+                                line = indent + 'Step("' + stepText + text + '"' + levelStr + ')\n'
                         procedure += '##    ' + stepText + text + '\n'
+                        parsedLines.append(line)
                 except:
-                    errorList.append([lineNumber, ' exception - ' + traceback.format_exc()])
+                    self.errorList.append([lineNumber, ' exception - ' + traceback.format_exc()])
                     status = 'NOK'
-
-            elif re.search(r'^[ \t]*Logs *\(', line):
-                try:
-                    line = re.sub(r'Logs *\( *" *', 'Logs("', line, 1)  # delete needless space chars in "Logs ( " string
-                    line = re.sub(r' *" *\) *$', '")', line, 1)  # delete needless space chars in " ) " string
-                    logsNumber += 1
-                    line = re.sub(r'(?<=[\'"]) *\d*\.* *', str(logsNumber) + '. ', line, 1)  # update test logs number
-                    procedure += '##    ' + re.search(r'(?<=[\'"]).*(?=[\'"])', line).group() + '\n'
-                except:
-                    errorList.append([lineNumber, ' exception - ' + traceback.format_exc()])
-                    status = 'NOK'
-
-            if line == '##    PROCEDURE:\n':
-                procedureFlag = True
-
-            if procedureFlag and not (line.isspace() or line.startswith('##    ')):
-                procedureFlag = False
-
-            if procedureFlag:  # delete procedure lines
-                lineNumberOffset -= 1
             else:
                 parsedLines.append(line)
+        self.temporary_file = parsedLines
 
-        if not reqirementFlag:
-            procedure += '#Req([])\n'
-
-        lineNumberOffset += procedure.count('\n')
-        for lineNumber, errorDescription in errorList:
-            if lineNumber > headerEndLineNumber:
-                lineNumber = lineNumber + lineNumberOffset
-            # print(("Error: " + fileName + ", line: " + str(lineNumber) + errorDescription))
-
-        # parsedLines.insert(headerEndLineNumber, procedure)
 
 
 if __name__ == '__main__':
